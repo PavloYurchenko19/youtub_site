12bb60913cb2f5b8f49f26fa2010c9b3b2af340c
Pablo19 <stolbunpavlo@gmail.com>
2022-12-07 11:42:05 +0200

    git

--- src/Component/Header/Header.tsx
+++ src/Component/Header/Header.tsx
@@ -29,6 +29,7 @@ const Header: FC = () => {
                 }
                 }
                 className={'header__container-img'}>
+
                 <img src='/logo.svg' alt="Logo"/>
             </motion.div>
 
